5294d9d83b78431291b17ebedbf9b1a67a4630d9
Katy Morton <krokmorton@gmail.com>
2019-08-07 18:29:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-function shout(){
-  
+
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+if (string.toLowerCase()===string){
+  return "Speak up, dear!"} 
+if (string.toUpperCase()===string){
+  return "YES INDEEDY!"}
+if (string === "I love you, Grandma."){
+  return "I love you, too."}
 }
