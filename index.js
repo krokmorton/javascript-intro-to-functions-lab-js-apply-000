8e1abf30336354ca9651af3f65359f84925653fe
Katy Morton <krokmorton@gmail.com>
2019-08-07 18:32:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string) {
-if (string.toLowerCase()===string){
+if (string === string.toLowerCase()){
   return "Speak up, dear!"} 
-if (string.toUpperCase()===string){
+if (string === string.toUpperCase()){
   return "YES INDEEDY!"}
 if (string === "I love you, Grandma."){
   return "I love you, too."}
